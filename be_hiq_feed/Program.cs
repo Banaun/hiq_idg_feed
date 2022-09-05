555e6a1f337c35b1d1dad67335613dae9226e0fc
Emil Mogensen <emil.mogensen87@gmail.com>
2022-09-05 16:15:54 +0200

    added simple get methods with relevant data

--- be_hiq_feed/Program.cs
+++ be_hiq_feed/Program.cs
@@ -1,19 +1,51 @@
+using System.Xml;
+using System.ServiceModel.Syndication;
+using Newtonsoft.Json;
+
+string url = "http://www.idg.se/rss/100+senaste?noredirect=true";
+XmlReader reader = XmlReader.Create(url);
+SyndicationFeed feed = SyndicationFeed.Load(reader);
+reader.Close();
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.MapGet("/", () => {
+    return feed;
+});
 
-app.UseHttpsRedirection();
+app.MapGet("/feed-info", () => {
+    return feed.Description.Text;
+});
+
+app.MapGet("/items", () => {
+
+    List<Article> articles = new List<Article>();
+
+    foreach (SyndicationItem item in feed.Items) {
+        Article article = new Article();
+        article.id = item.Id;
+        article.title = item.Title.Text;
+        article.summary = item.Summary.Text;
+        article.publishDate = item.PublishDate;
+        /*
+        foreach (SyndicationCategory attribute in item.Categories) {
+            article.category.Add(attribute.Name);
+        }
+        */
+
+        articles.Add(article);
+    }
+
+    return articles;
+});
 
 app.Run();
+
+/*
+string json = JsonConvert.SerializeObject(new {
+    articleList = new List<Article>() {
+        new Article { id = 1, title = "ABC", description = "ABC", pubDate = "Mon, 5 Sep 2022 11:32:59" }
+    }
+});
+*/
