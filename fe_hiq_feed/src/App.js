444267f2b975b4bc856b7969d648fe7fb44e9891
Emil Mogensen <emil.mogensen87@gmail.com>
2022-09-05 16:37:48 +0200

    added fetch method and simple seperation of data

--- fe_hiq_feed/src/App.js
+++ fe_hiq_feed/src/App.js
@@ -1,10 +1,21 @@
+import React, { useState, useEffect } from "react";
 
+const App = () => {
 
-function App() {
-  return (
-    <div className="main-container">
-    </div>
-  );
+  useEffect(() => {
+    let articles = [" "];
+
+    async function fetchArticles() {
+      let url = "https://localhost:7293/items"
+      let response = await fetch(url);
+      let responseAsJson = await response.json();
+      for (let i = 0; i < responseAsJson.length; i++) {
+        articles.push(responseAsJson[i])
+      }
+      console.log(articles)
+    }
+    fetchArticles();
+  }, []);
 }
 
 export default App;
