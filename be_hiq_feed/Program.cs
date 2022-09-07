bfc459714400e85a9f8612f4d7fd055ea43a668c
Emil Mogensen <emil.mogensen87@gmail.com>
2022-09-07 15:21:39 +0200

    fixed bug that sometimes clipped sentences in article description

--- be_hiq_feed/Program.cs
+++ be_hiq_feed/Program.cs
@@ -41,9 +41,10 @@ app.MapGet("/items", () => {
         article.id = item.Id;
         article.title = item.Title.Text;
         string[] words = item.Summary.Text.Split('"');
-        string[] summary = words[4].Split('>');
+        string[] summary = item.Summary.Text.Split('>');
+        //string[] summary = words[4].Split('>');
         article.imageUrl = words[1];
-        article.description = summary[2];
+        article.description = summary.Last();
         article.publishDate = item.PublishDate;
 
         article.category = new List<string>();
