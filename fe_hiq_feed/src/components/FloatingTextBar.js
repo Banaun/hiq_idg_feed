8bc08a69d1ff48ebcc1398d8ec44e427157a2246
Emil Mogensen <emil.mogensen@hiq.se>
2022-11-07 12:15:17 +0100

    fixed floating header

--- fe_hiq_feed/src/components/FloatingTextBar.js
+++ fe_hiq_feed/src/components/FloatingTextBar.js
@@ -1,43 +1,72 @@
-import React, { useState, useEffect } from "react";
-import Marquee from "react-fast-marquee";
+import React, { useRef, useState, useEffect } from 'react';
 
 const FloatingTextBar = () => {
-    const [headlineList, setHeadlineList] = useState([]);
+  const [headlineList, setHeadlineList] = useState([]);
+  const headlines = useRef();
 
-    //Fetch headlines from backend and populate the headlines array
-    const getHeadlines = async () => {
-        let headlines = []
+  //Fetch headlines from backend and populate the headlines array
+  const getHeadlines = async () => {
+    let headlines = [];
 
-        let url = "https://behiqfeed.azurewebsites.net/latest";
-        let response = await fetch(url, {
-            mode: 'cors',
-          });
-        let responseAsJson = await response.json();
-        for (let i = 0; i < responseAsJson.length; i++) {
-            headlines.push(responseAsJson[i]);
-          }
-          setHeadlineList(headlines);
+    let url = 'https://behiqfeed.azurewebsites.net/latest';
+    let response = await fetch(url, {
+      mode: 'cors',
+    });
+    let responseAsJson = await response.json();
+    for (let i = 0; i < responseAsJson.length; i++) {
+      headlines.push(responseAsJson[i]);
     }
+    let repeatedHeadlines = Array(5).fill(headlines).flat();
 
-    useEffect(() => {
-        getHeadlines();
-      }, []);
+    setHeadlineList(repeatedHeadlines);
+  };
 
-    return (
-        <Marquee className="floating-text" gradient={false} speed="10">
-            {headlineList.map((headline) => (
-                <>
-                {headline.containsKeyword 
-                    ? 
-                    <p className="headline-important" key={headline.id}>{headline.title}</p> 
-                    :
-                    <p className="headline" key={headline.id}>{headline.title}</p>
-                }
-                <p>&nbsp;&nbsp;{"|"}&nbsp;&nbsp;</p>
-                </>
-            ))}
-        </Marquee>
-    )
-}
+  useEffect(() => {
+    getHeadlines();
+  }, []);
+
+  useEffect(() => {
+    animate(headlines);
+  }, [headlines]);
+
+  function animate(headlines) {
+    let elementWidth = headlines.current.offsetWidth;
+    let parentWidth = headlines.current.parentElement;
+    let flag = 0;
+
+    setInterval(() => {
+      headlines.current.style.marginLeft = --flag + 'px';
+
+      if (elementWidth == -flag) {
+        flag = parentWidth;
+      }
+    }, 20);
+  }
+
+  return (
+    <div className='floating-text-header'>
+      <div
+        ref={headlines}
+        className='floating-text'
+        style={{ marginLeft: '0px' }}
+      >
+        {headlineList.map((headline) => (
+          <>
+            {headline.containsKeyword ? (
+              <p className='headline-important' key={headline.id}>
+                {headline.title}
+              </p>
+            ) : (
+              <p className='headline' key={headline.id}>
+                {headline.title}
+              </p>
+            )}
+            <p style={{ fontWeight: 'bold' }}>&nbsp;&nbsp;{'|'}&nbsp;&nbsp;</p>
+          </>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default FloatingTextBar;
